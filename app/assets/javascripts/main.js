3296eaac4f79fe13b11e92683d09df3b70956ece
vishaljangir <vishaljangir022@gmail.com>
2018-12-26 17:45:42 +0530

    grand total update

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,45 @@
+//for grand total
+function grandTotal(){
+  var subtotal = 0;
+    $('.total').each(function(i){
+        var price = $(this).html();
+        if (!isNaN(price)) subtotal += Number(price);
+    });
+    subtotal = subtotal.toFixed(2);
+    $('.grandTotal').html(subtotal);
+}
+
+//for addition
+function addition(){
+    var dome = Number(document.getElementById("dome").value);
+    $('.domeQty').val(dome);
+
+    var bullet = Number(document.getElementById("bullet").value);
+    $('.bulletQty').val(bullet);
+    var add = (dome + bullet);
+    document.getElementById('demo').innerHTML = add;
+}
+
+//for price and total 
+ jQuery(function() {
+    return $(document).on("change",".cctv", function(){
+      selectedBrand = $(this).parent('td').prev('td').text();
+      // alert(selectedBrand);
+      selectedCategory = $(this).val();
+      // alert(selectedCategory);
+      selectProducts = cctvsPrice[selectedBrand + ":" + selectedCategory];
+      productPrice = $(this).parent('td').next('td').children('.price');
+      productPrice.empty();
+
+      var price = selectProducts[0].price;
+      $(this).parent('td').next('td').children('.price').val(price);
+      var qty = $(this).parent('td').next('td').next('td').children('.qty').val();
+      var place_total = $(this).parent('td').next('td').next('td').next('td').children('.total');
+      var total = price * qty;
+      place_total.html(total);
+      
+      grandTotal();
+ 
+    });
+    
+  });
